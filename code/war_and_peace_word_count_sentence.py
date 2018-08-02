d2e68c3e54fd86469e43892d583c73dd058039e6
Paul Wu <zhou.wu@northseattlecolleges.edu>
2018-08-02 06:50:50 -0700

    Count sentences.

---
+++ code/war_and_peace_word_count_sentence.py
@@ -0,0 +1,26 @@
+'''
+
+Week3 Word count example.
+
+By Paul Wu uwzwu@uw.edu
+
+'''
+
+from __future__ import print_function
+import sys
+import re
+
+from util import spark, sc
+import time
+start_time = time.time()
+
+text  =  sc.textFile("data/war_and_peace.txt")
+words  =  text.flatMap(lambda  line  :  re.split('.', line.lower()))
+peace_and_war = text.filter(lambda  line: 'peace' in  re.split('\W+', line.lower()) and 'war' in re.split('\W+', line.lower()))
+wordsPairs  =  words.map(lambda  word  :  (word,1))
+counts  =  wordsPairs.reduceByKey(lambda  value1,value2  :  value1  +  value2)
+counts = counts.sortBy(lambda pair: -pair[1])
+counts.foreach(print)
+
+sc.stop()
+print("--- %s seconds ---" % (time.time() - start_time))
